f97c816e387f5cb118279691d4b6ae17c0cb6c55
Infinity2209 <anantanag91@gmail.com>
2025-07-20 09:46:56 +0530

    tag is added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,16 @@ import homeLogo from '../assets/about.png'
 import Particle from '../components/Particle';
 import About from '../components/Home/About';
 import Type from '../components/Home/Type';
+import Tags from '../components/Home/Tags';
 
 const Home = () => {
+  const tags = ["Software Engineer", "Developer", " Website Designer", "Tech Enthusiast", "Full Stack Developer", "UI/UX Designer", "AI Enthusiast", "Web Developer", "Creative Thinker", "Writer", "Shayer"];
+
   return (
     <section>
       <Container fluid className="home-section" id="home">
         <Particle />
-      <Container fluid className="home-content" style={{ marginLeft: 0, marginRight: 0 }}>
+        <Container fluid className="home-content" style={{ marginLeft: 0, marginRight: 0 }}>
           <Row>
             <Col md={7} className="home-header">
               <h1 style={{ paddingBottom: 15 }} className="heading">
@@ -41,6 +44,9 @@ const Home = () => {
           </Row>
         </Container>
       </Container>
+      <div className="tag-compo">
+        <Tags tags={tags} />
+      </div>
       <About />
     </section>
   );
